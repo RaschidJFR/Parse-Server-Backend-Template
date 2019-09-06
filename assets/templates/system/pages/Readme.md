dd3088ff4e4b7d88e063a1e4177cedd4d8d0bec4
Raschid J.F. Rafaelly <me@raschidjfr.dev>
2019-09-05 19:20:29 -0500

    feat(parse-server.config): add support for customPages

---
+++ assets/templates/system/pages/Readme.md
@@ -0,0 +1,3 @@
+In this folder you'll find the templates for the user-face pages (reset password, email verified, etc).
+
+If you're hosting you're app on [Back4app](https://back4app.com) (or maybe other service) you may need to contact the support team to ask them to get these installed.
--- config/parse-server.config.js
+++ config/parse-server.config.js
@@ -1,18 +1,24 @@
 
-// Add here your app's core config var
+// Add here your app's core config variables
 
 const APP_ID = 'app-id';
 const MASTER_KEY = 'master-key';
 const MOUNT_PATH = '/parse';
+const PUBLIC_URL = `http://localhost:1337${MOUNT_PATH}`;
+const PAGE_TEMPLATE_PUBLIC_URL = '//localhost:7007/action';
 
 // Configure your prod and dev databases in the file `databases.json`
 
-let DEV_DATABASE = `mongodb://localhost:27017${MOUNT_PATH}`;
+let DATABASES_CONFIG = {
+  dev: `mongodb://localhost:27017${MOUNT_PATH}`,
+  prod: `mongodb://localhost:27017${MOUNT_PATH}`
+}
 
 try {
-  DEV_DATABASE = require('./databases.json').dev;
+  DATABASES_CONFIG = require('./databases.json');
 } catch (e) {
-  console.warn('Could not read file config/databases.json. Using default database uri: %s\n', DEV_DATABASE);
+  console.warn('Could not read file config/databases.json. Using default database uri: %s\n. Expected file content:\n\x1b[0m\n%o\n',
+    DATABASES_CONFIG.dev, DATABASES_CONFIG);
 }
 
 
@@ -21,13 +27,13 @@ try {
 let MAILGUN_CONFIG = {
   apiKey: 'yourmailgunapikey',
   domain: 'noreply@mg.yourdomain.com',
-  from:'MyApp Bot'
+  from: 'MyApp Bot'
 }
 
 try {
   MAILGUN_CONFIG = require('./credentials/mailgun.json');
 } catch (e) {
-  console.error('\x1b[33mCould not read file \'config/credentials/mailgun.json\'. Example:\n\x1b[0m\n%o\n', MAILGUN_CONFIG);
+  console.error('\x1b[33mCould not read file \'config/credentials/mailgun.json\'. Expected file content:\n\x1b[0m\n%o\n', MAILGUN_CONFIG);
 }
 
 // Export configuration
@@ -38,9 +44,22 @@ module.exports = {
   masterKey: MASTER_KEY,
   cloud: './build/cloud/main.js',
   mountPath: MOUNT_PATH,
-  databaseURI: DEV_DATABASE,
-  serverURL: `http://localhost:1337${MOUNT_PATH}`,
-  publicServerURL: `http://localhost:1337${MOUNT_PATH}`,
+  databaseURI: DATABASES_CONFIG.dev,
+  serverURL: PUBLIC_URL,
+  publicServerURL: PUBLIC_URL,
+
+  // Custom Pages
+  customPages: {
+    choosePassword: `${PAGE_TEMPLATE_PUBLIC_URL}/password_reset.html`,
+    invalidLink: `${PAGE_TEMPLATE_PUBLIC_URL}/invalid_link.html`,
+    invalidVerificationLink: `${PAGE_TEMPLATE_PUBLIC_URL}/invalid_verification_link.html`,
+    linkSendFail: `${PAGE_TEMPLATE_PUBLIC_URL}/link_send_fail.html`,
+    linkSendSuccess: `${PAGE_TEMPLATE_PUBLIC_URL}/link_send_success.html`,
+    passwordResetSuccess: `${PAGE_TEMPLATE_PUBLIC_URL}/password_reset_success.html`,
+    verifyEmailSuccess: `${PAGE_TEMPLATE_PUBLIC_URL}/verify_email_success.html`,
+  },
+
+  // Custom Email Templates
   emailAdapter: {
     module: 'parse-server-mailgun',
     options: {
@@ -62,6 +81,7 @@ module.exports = {
       }
     }
   },
+
   verbose: process.env.NODE_ENV === 'development',
   verifyUserEmails: true,
   preventLoginWithUnverifiedEmail: false
